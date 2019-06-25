ba368c86f41cd6809ea5fecc942e6c689baab0c8
Vibha Yadav <vyadav477@gmail.com>
2019-06-25 19:05:41 +0530

    changes

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+class Home extends React.Component{
+
+    render(){   
+        return();
+    }
+}
+
+
+ReactDOM.render(<Home />, document.getElementById("root"));
+
+
+
