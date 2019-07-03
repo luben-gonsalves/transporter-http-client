b52e5c388f83ab81ef3253874c567ed9915fe8d3
luben <lubenfrank48@gmail.com>
2019-07-03 16:39:12 +0530

    fixed conflicts

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { InputQuery } from "./inputUrl";
+import Collection from "./collections";
+
+class App extends React.Component {
+  render() {
+    return <Collection />;
+  }
+}
+
+export default App;
