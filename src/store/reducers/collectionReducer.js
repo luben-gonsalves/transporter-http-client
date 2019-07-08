2f9f9277fb7af9332755e15870c0146cd77c4dd1
luben <lubenfrank48@gmail.com>
2019-07-08 17:15:11 +0530

    localstorage for storing and retrieving data for collection

--- src/store/reducers/collectionReducer.js
+++ src/store/reducers/collectionReducer.js
@@ -1,20 +1,21 @@
-function collectionReducer(
-  collection = [
-    { collectionName: "collecti 1", requests: [] },
-    { collectionName: "collecti 2", requests: [] },
-    { collectionName: "collecti 3", requests: [] }
-  ],
-  action
-) {
+var localCollection = localStorage.getItem("collection");
+if (!localCollection) {
+  localCollection = [];
+}
+localCollection = JSON.parse(localCollection);
+
+function collectionReducer(collection = localCollection, action) {
   if (action.type === "CREATE_COLLECTION") {
     let state = collection.slice();
     state.push(action.collectionData);
+    localStorage.setItem("collection", JSON.stringify(state));
     return state;
   }
 
   if (action.type === "DELETE_COLLECTION") {
     let state = collection.slice();
     state.splice(action.index, 1);
+    localStorage.setItem("collection", JSON.stringify(state));
     return state;
   }
 
@@ -23,18 +24,26 @@ function collectionReducer(
     state[action.editData.index].collectionName =
       action.editData.collectionName;
     state[action.editData.index].description = action.editData.description;
+    localStorage.setItem("collection", JSON.stringify(state));
     return state;
   }
 
   if (action.type === "ADD_REQUEST") {
     let state = collection.slice();
-    state[action.index].requests.push(action.requestData);
+    if (Object.keys(state).includes("requests")) {
+      state[action.index].requests.push(action.requestData);
+    } else {
+      state[action.index].requests = [];
+      state[action.index].requests.push(action.requestData);
+    }
+    localStorage.setItem("collection", JSON.stringify(state));
     return state;
   }
 
   if (action.type === "DELETE_REQUEST") {
     let state = collection.slice();
     state[action.collectionIndex].requests.splice(action.requestIndex, 1);
+    localStorage.setItem("collection", JSON.stringify(state));
     return state;
   }
 
