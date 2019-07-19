18206806168fb2fe09568e0e5c2da964c915870c
Vibha Yadav <vyadav477@gmail.com>
2019-07-19 15:58:57 +0530

    import

--- src/components/historyData.js
+++ src/components/historyData.js
@@ -3,62 +3,62 @@ import { stateMapper } from "../store/store";
 import { connect } from "react-redux";
 
 class HistoryData extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showMore: false
-        };
-        this.handleButton = this.handleButton.bind(this);
-        this.historyData = this.historyData.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMore: false
+    };
+    this.handleButton = this.handleButton.bind(this);
+    this.historyData = this.historyData.bind(this);
+  }
 
-    handleButton() {
-        this.state.showMore
-            ? this.setState({
-                showMore: false
-            })
-            : this.setState({
-                showMore: true
-            });
-    }
-    historyData() {
-        console.log(this.props.hdata)
+  handleButton() {
+    this.state.showMore
+      ? this.setState({
+        showMore: false
+      })
+      : this.setState({
+        showMore: true
+      });
+  }
+  historyData() {
+    console.log(this.props.hdata)
 
-        if (this.props.hdata.requests && this.state.showMore) {
+    if (this.props.hdata.requests && this.state.showMore) {
 
-            return this.props.hdata.requests.map(b => {
-                return (
-                    <div>
-                        <button type="button" class="btn btn-secondary btn-sm">{b.requestName}</button>
-                        <br />
-                        <br />
-                    </div>
-
-                )
-            })
-        }
-    }
-
-    render() {
+      return this.props.hdata.requests.map(b => {
         return (
-            <div >
-                <div class="btn-group">
+          <div>
+            <button type="button" class="btn btn-secondary btn-sm">{b.requestName}</button>
+            <br />
+            <br />
+          </div>
 
-                    <button type="button" onClick={this.handleButton} class="btn btn-secondary btn-sm">{this.props.hdata.historyName}</button>
-
-                    <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="sr-only">Toggle Dropdown</span>
-                    </button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Remove</a>
-                    </div>
-                </div>
-                <br />
-                <br />
-                {this.historyData()}
-            </div>
-        );
+        )
+      })
     }
+  }
+
+  render() {
+    return (
+      <div >
+        <div class="btn-group">
+
+          <button type="button" onClick={this.handleButton} class="btn btn-secondary btn-sm">{this.props.hdata.historyName}</button>
+
+          <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="sr-only">Toggle Dropdown</span>
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Remove</a>
+          </div>
+        </div>
+        <br />
+        <br />
+        {this.historyData()}
+      </div>
+    );
+  }
 }
 
 export default HistoryData;
